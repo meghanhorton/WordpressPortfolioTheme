d70753321a548fd964299c65a92308471037504a
Meghan Welford <mlwelford@gmail.com>
2014-03-29 12:39:23 -0400

    Added jQuery for Portfolio Item hover action

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,14 @@
-$('.carousel').carousel();
+jQuery(document).ready( 
+	function(){
+		jQuery('.portfolioitem').hover(
+			function(){
+				jQuery(this).parent('section').prepend('<div class="portfolio-overlay">Test</div>');
+			}
+		);
+		jQuery('.portfolioitem').mouseout(
+			function(){
+				jQuery('.portfolio-overlay').remove();
+			}
+		);
+	}
+);
